eabf4f69f4680edcc79a1662cb4052927e186a36
ABDOHAMDA <deboabdo1234@gmail.com>
2026-05-07 18:58:46 +0300

    edit files HackMe

--- update_labs.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once __DIR__ . '/server/core/db/db_connect.php';
-$conn->query("UPDATE labs SET is_published = 1, visibility = 'public' WHERE lab_id IN (11, 46)");
-echo "Updated: " . $conn->affected_rows . "\n";
